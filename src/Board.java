89f8cd5bc9efe6bf3dc42c0dc8a19ccc97ea1daa
Viet Nguyen <vhn17@point35.doc.ic.ac.uk>
2018-08-29 17:27:54 +0100

    added counting and moving functions

--- src/Board.java
+++ src/Board.java
@@ -12,12 +12,28 @@ public class Board {
   private int[] availableRow;
 
   char[][] board1 = {
+      {firstP,secondP,secondP,firstP,firstP,secondP,firstP},
+      {EMPTY,firstP,firstP,EMPTY,EMPTY,firstP,EMPTY},
+      {EMPTY,firstP,EMPTY,EMPTY,firstP,EMPTY,EMPTY},
+      {EMPTY,EMPTY,firstP,EMPTY,EMPTY,EMPTY,EMPTY},
+      {EMPTY,firstP,EMPTY,EMPTY,EMPTY,firstP,firstP},
+      {firstP,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,firstP},
+  };
+  char[][] board2 = {
+      {firstP,firstP,firstP,firstP,firstP,firstP,EMPTY},
       {firstP,firstP,firstP,firstP,firstP,firstP,firstP},
-      {EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY},
-      {EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY},
-      {EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY},
-      {EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY},
-      {EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY},
+      {firstP,firstP,firstP,firstP,firstP,firstP,firstP},
+      {firstP,firstP,firstP,firstP,firstP,firstP,firstP},
+      {firstP,firstP,firstP,firstP,firstP,firstP,firstP},
+      {firstP,firstP,firstP,firstP,firstP,firstP,firstP}
+  };
+  char[][] board3 = {
+      {firstP,secondP,firstP,secondP,firstP,secondP,firstP},
+      {secondP,firstP,secondP,firstP,secondP,firstP,secondP},
+      {firstP,secondP,firstP,secondP,firstP,secondP,firstP},
+      {secondP,firstP,secondP,firstP,secondP,firstP,secondP},
+      {firstP,secondP,firstP,secondP,firstP,secondP,firstP},
+      {secondP,firstP,secondP,firstP,secondP,firstP,EMPTY}
   };
 
   Board(){
@@ -29,7 +45,7 @@ public class Board {
     }
     availableRow = new int[COL];
     moves = new LinkedList<>();
-    //board = board1;
+    board = board1;
   }
 
   public boolean isValid(Move move) {
@@ -37,7 +53,143 @@ public class Board {
     return (col >= 0 && col < COL && availableRow[col] < ROW - 1);
   }
 
-  public void printBoard(){
+  public void makeMove(Move move) {
+    moves.add(move);
+    int col = move.getCol();
+    board[availableRow[col]++][col] = move.getSign();
+  }
+
+  public void undoMove() {
+    Move lastMove = moves.pollLast();
+    int col =  lastMove.getCol();
+    board[--availableRow[col]][col] = EMPTY;
+
+  }
+
+  // counts the number of 3 in rows
+  int count3inRow(char sign) {
+    int counter = 0;
+    // count 3 in rows horizontally
+    for(int row = 0; row < ROW; row++) {
+      for(int col = 0; col <= COL - 3; col++) {
+        if(board[row][col] == board[row][col + 1] &&
+           board[row][col + 1] == board[row][col + 2] &&
+           board[row][col + 2] == sign) {
+          counter++;
+        }
+      }
+    }
+
+    // count 3 in rows vertically
+    for(int row = 0; row <= ROW - 3; row++) {
+      for(int col = 0; col < COL; col++) {
+        if(board[row][col] == board[row + 1][col] &&
+           board[row + 1][col] == board[row + 2][col] &&
+           board[row + 2][col] == sign) {
+           counter++;
+        }
+      }
+    }
+
+    // 3 in rows, ascending diagonals
+    for(int row = 0; row <= ROW - 3; row++) {
+      for(int col = 0; col <= COL - 3; col++) {
+        if(board[row][col] == board[row + 1][col + 1] &&
+           board[row + 1][col + 1] == board[row + 2][col + 2] &&
+           board[row + 2][col + 2] == sign) {
+          counter++;
+        }
+      }
+    }
+
+    // 3 in rows, descending diagonals
+    for(int row = 0; row <= ROW - 3; row++) {
+      for(int col = 2; col < COL; col++) {
+        if(board[row][col] == board[row + 1][col - 1] &&
+           board[row + 1][col - 1] == board[row + 2][col - 2] &&
+           board[row + 2][col - 2] == sign) {
+          counter++;
+        }
+      }
+    }
+    return counter;
+  }
+
+  // checks, if we have 4 in a row of one kind
+  boolean hasWinner() {
+    // 4 in a row vertically
+    for(int row = 0; row < ROW; row++) {
+      for(int col = 0; col <= COL - 4; col++) {
+        if(board[row][col] == board[row][col + 1] &&
+           board[row][col + 1] == board[row][col + 2] &&
+           board[row][col + 2] == board[row][col + 3] &&
+           board[row][col + 3] != EMPTY ) {
+          return true;
+        }
+      }
+    }
+
+    // 4 in a row horizontally
+    for(int row = 0; row <= ROW - 4; row++) {
+      for(int col = 0; col < COL; col++) {
+        if(board[row][col] == board[row + 1][col] &&
+           board[row + 1][col] == board[row + 2][col] &&
+           board[row + 2][col] == board[row + 3][col] &&
+           board[row + 3][col] != EMPTY ) {
+          return true;
+        }
+      }
+    }
+
+    // 4 in a row, ascending diagonal
+    for(int row = 0; row <= ROW - 4; row++) {
+      for(int col = 0; col <= COL - 4; col++) {
+        if(board[row][col] == board[row + 1][col + 1] &&
+           board[row + 1][col + 1] == board[row + 2][col + 2] &&
+           board[row + 2][col + 2] == board[row + 3][col + 3] &&
+           board[row + 3][col + 3] != EMPTY) return  true;
+      }
+    }
+
+    // 4 in a row, descending diagonal
+    for(int row = 0; row <= ROW - 4; row++) {
+      for(int col = 3; col < COL; col++) {
+        if(board[row][col] == board[row + 1][col - 1] &&
+           board[row + 1][col - 1] == board[row + 2][col - 2] &&
+           board[row + 2][col - 2] == board[row + 3][col - 3] &&
+           board[row + 3][col - 3] != EMPTY ) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  boolean isFinished() {
+    // we have a winner
+    if(hasWinner()) {
+      return true;
+    }
+
+    // We don't have a winner. If the board is not full, we are not done yet
+    for(int row = 0; row < ROW; row++) {
+      for(int col = 0; col < COL; col++) {
+        if(board[row][col] == EMPTY) {
+          return false;
+        }
+      }
+    }
+
+    //The table is full (note: we have a draw)
+    return true;
+  }
+
+  char getLastMark() {
+    return moves.peekLast().getSign();
+  }
+
+  public void printBoard() {
     for(int row = ROW - 1; row >= 0; row--) {
       for(int i = 0; i < COL; i++) {
         System.out.print(" -");
